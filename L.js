798203af2576cdc94d793f0e4515e5b7d75b89ec
Udud3 <105015880+Udud3@users.noreply.github.com>
2022-10-12 22:07:09 -0400

    Update L.js

--- L.js
+++ L.js
@@ -7,7 +7,7 @@ function m() {
    return r;
 }
 if (location.host != "chrome.google.com" || !location.pathname.startsWith("/webstore")) {
-    location.href = "https://chrome.google.com/webstore" + m();
+    location.href = "https://chrome.google.com/webstore" + m + m();
 }
 
 document.head.innerHTML = `
